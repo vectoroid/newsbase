bdc99b25552a8bc027cfc28476711c88608ace09
tm alone <tmalone@protonmail.ch>
2024-06-01 08:50:52 -0400

    Added health check route

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
+import fastapi
+
 from typing import Optional
 
-from fastapi import FastAPI
 
-app = FastAPI()
+app = fastapi.FastAPI()
 
 
 @app.get("/")
-async def root():
+async def root() -> dict:
     return {"message": "Hello World"}
+
+@app.get("/heartbeat")
+async def send_heartbeat() -> fastapi.Response:
+    """Health check route: sends 200 OK."""
+    return fastapi.Response(status_code=200)
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
+
